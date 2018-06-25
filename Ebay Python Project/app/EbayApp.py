e068814965b6b4c55b84b3ce33e99e308cf884e3
rtl251 <39318405+rtl251@users.noreply.github.com>
2018-06-25 00:34:21 -0400

    Update EbayApp.py

--- Ebay Python Project/app/EbayApp.py
+++ Ebay Python Project/app/EbayApp.py
@@ -14,7 +14,6 @@ load_dotenv()
 
 #connecting to Ebay API key from env file
 api = finding(appid=(os.environ.get("api_key")),headers=str(), config_file=None)
-#api = finding(appid="RobertLa-4-PRD-d66850dff-b909ea31", config_file=None)
 
 #Asks user what they want to search for in ebay
 search_words = input("Enter the name of item for which you want to view recently sold data on Ebay:")
@@ -22,6 +21,8 @@ print("")
 print(f"Enter \"1\" to see sold listings related to {search_words} that are new,")
 print(f"Enter \"2\" to see sold listings related to {search_words} that are used,")
 condition_input = input(f"Or enter \"3\" to see sold listings related to {search_words} that are both new and used (unspecified):")
+
+#Error Handling
 accepted_inputs=["1","2","3"]
 while condition_input not in accepted_inputs:
     condition_input=input("Input not recognized. Please enter \"1\", \"2\", or \"3\"")
@@ -35,9 +36,11 @@ else:
 
 condition_value_format=condition_value.lower()
 
+#Start request and notify user
 print("")
 print(f"Generating list of most recent sold auctions related to {search_words} in {condition_value_format} condition...")
 
+#Incase user has api_key issues
 try:
 
 #Full input request to ebay- search term, 100 results (if applicable), sorted by recently sold, new items only, located in U.S. only, Sold Items Only, currency in USD
@@ -62,7 +65,6 @@ try:
     file_name = search_words + "_" + condition_value_format + "_" + timestamp + ".csv"
 
 
-#print(souptext)
     ID=[]
     for a in soup.find_all("itemid"):
         id_int=a.text
@@ -88,17 +90,21 @@ try:
         item_price_usd = "${0:,.2f}".format(item_price)
         Sold_Price.append(item_price_usd)
 
+        #creates dictinary
         ebay_history = [{"Ebay ID":ID,"Description": Description,"Sold_Date":Sold_Date,"Sold_Price":Sold_Price} for ID, Description, Sold_Date, Sold_Price
         in zip(ID, Description, Sold_Date, Sold_Price)]
 
+        #write to csv
         with open(file_name, "w",  encoding="utf-8", newline="") as csv_file:
             writer = csv.DictWriter(csv_file, fieldnames=["Ebay ID","Description","Sold_Date","Sold_Price"])
             writer.writeheader()
             for soldlisting in ebay_history:
                 writer.writerow(soldlisting)
-
+                
+    #notify user request & reciept of data is donw
     print("List generation complete. A csv file with the most recent sold data is now available.")
 
+#inform user about ebaysdk-request issue
 except:
     print("ERROR - There is a known issue with ebaysdk and making requests,")
     print(" preventing this program from recognizing the api_key in the .env file: https://github.com/timotheus/ebaysdk-python/issues/162")
